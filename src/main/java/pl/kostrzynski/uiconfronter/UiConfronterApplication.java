f71dca23fb32b1927a6384d1085ff8c2a521be14
Rafal Kostrzynski <kostrzynskirafal@wp.pl>
2020-12-27 14:07:05 +0100

    init

---
+++ src/main/java/pl/kostrzynski/uiconfronter/UiConfronterApplication.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.uiconfronter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiConfronterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiConfronterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kostrzynski/uiconfronter/UiConfronterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.uiconfronter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiConfronterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
