a338e5f2012049919996c32c05c793c7016e6b95
Rafal Kostrzynski <kostrzynskirafal@wp.pl>
2021-01-22 15:38:06 +0100

    deployment

--- uiconfronterapp/src/app/http-service.service.ts
+++ uiconfronterapp/src/app/http-service.service.ts
@@ -3,37 +3,38 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {PageInfo} from "./models/PageInfo";
 import {Observable} from "rxjs";
 import {PageEntity} from "./models/pageEntity";
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
 
-  private BACKEND_URL = 'http://r-backend-labproj20.apps.cp4apps.cloudpak.site';
+  API_URL = environment.apiUrl;
 
   constructor(private http: HttpClient) {
   }
 
   getCompareData(pageUrl: string, date: Date): Observable<PageInfo[]> {
     let params = new HttpParams().set("url", pageUrl).set("localDate", date.toISOString().substr(0,10));
-    return this.http.get<PageInfo[]>(this.BACKEND_URL+"/UI-Confronter/compare-info", {params: params});
+    return this.http.get<PageInfo[]>(this.API_URL+"/UI-Confronter/compare-info", {params: params});
   }
 
   getOlderWebsite(pageUrl: string, date: Date): Observable<any> {
     let params = new HttpParams().set("url", pageUrl).set("localDate", date.toISOString().substr(0,10));
-    return this.http.get(this.BACKEND_URL+"/UI-Confronter/get-older-link", {params: params});
+    return this.http.get(this.API_URL+"/UI-Confronter/get-older-link", {params: params});
   }
 
   getAllSavedWebsites(): Observable<any>{
-    return this.http.get(this.BACKEND_URL+"/UI-Confronter/all");
+    return this.http.get(this.API_URL+"/UI-Confronter/all");
   }
 
   saveData(oldPage:string, newPage:string){
     let savePage = new PageEntity(oldPage,newPage);
-    return this.http.post(this.BACKEND_URL+"/UI-Confronter",savePage);
+    return this.http.post(this.API_URL+"/UI-Confronter",savePage);
   }
 
   deleteData(id:number){
-    return this.http.delete(this.BACKEND_URL+"/UI-Confronter/"+id);
+    return this.http.delete(this.API_URL+"/UI-Confronter/"+id);
   }
 }
---
+++ uiconfronterapp/src/assets/env.js
@@ -0,0 +1,6 @@
+(function(window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["apiUrl"] = "http://r-backend-labproj20.apps.cp4apps.cloudpak.site";
+})(this);
