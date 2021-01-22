299ce63afc796c198fb43d435247bea00040becb
Rafal Kostrzynski <kostrzynskirafal@wp.pl>
2021-01-22 14:09:01 +0100

    deployment

--- uiconfronterapp/src/app/http-service.service.ts
+++ uiconfronterapp/src/app/http-service.service.ts
@@ -9,29 +9,31 @@ import {PageEntity} from "./models/pageEntity";
 })
 export class HttpServiceService {
 
+  private BACKEND_URL = 'http://r-backend-labproj20.apps.cp4apps.cloudpak.site';
+
   constructor(private http: HttpClient) {
   }
 
   getCompareData(pageUrl: string, date: Date): Observable<PageInfo[]> {
     let params = new HttpParams().set("url", pageUrl).set("localDate", date.toISOString().substr(0,10));
-    return this.http.get<PageInfo[]>("http://localhost:8080/UI-Confronter/compare-info", {params: params});
+    return this.http.get<PageInfo[]>(this.BACKEND_URL+"/UI-Confronter/compare-info", {params: params});
   }
 
   getOlderWebsite(pageUrl: string, date: Date): Observable<any> {
     let params = new HttpParams().set("url", pageUrl).set("localDate", date.toISOString().substr(0,10));
-    return this.http.get("http://localhost:8080/UI-Confronter/get-older-link", {params: params});
+    return this.http.get(this.BACKEND_URL+"/UI-Confronter/get-older-link", {params: params});
   }
 
   getAllSavedWebsites(): Observable<any>{
-    return this.http.get("http://localhost:8080/UI-Confronter/all");
+    return this.http.get(this.BACKEND_URL+"/UI-Confronter/all");
   }
 
   saveData(oldPage:string, newPage:string){
     let savePage = new PageEntity(oldPage,newPage);
-    return this.http.post("http://localhost:8080/UI-Confronter",savePage)
+    return this.http.post(this.BACKEND_URL+"/UI-Confronter",savePage);
   }
 
   deleteData(id:number){
-    return this.http.delete("http://localhost:8080/UI-Confronter/"+id);
+    return this.http.delete(this.BACKEND_URL+"/UI-Confronter/"+id);
   }
 }
