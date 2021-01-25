bd7f718a06ecd2f82d1c2f359ae9e6a36d0c59f6
Rafal Kostrzynski <kostrzynskirafal@wp.pl>
2021-01-25 08:31:17 +0100

    yaml for deployment

--- uiconfronterapp/src/environments/environment.prod.ts
+++ uiconfronterapp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: window["env"]["apiUrl"] || "http://DSAFSFAFASF"
+  apiUrl: window["env"]["apiUrl"] || "http://r-backend-labproj20.apps.cp4apps.cloudpak.site"
 };
--- uiconfronterapp/src/environments/environment.ts
+++ uiconfronterapp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: window["env"]["apiUrl"] || "http://"
+  apiUrl: window["env"]["apiUrl"] || "localhost"
 };
 
 /*
